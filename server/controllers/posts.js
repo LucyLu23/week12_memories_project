66a49d1958b4466f688fbe3fc9eaebb38852c44f
LucyLu23 <114943897+LucyLu23@users.noreply.github.com>
2023-01-20 16:26:18 -0700

    halfofvideopart2

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 
@@ -19,7 +20,7 @@ export const createPost = async (req, res) => {
     try {
         await newPost.save();
         // https://www.restapitutorial.com/httpstatuscodes.html
-        res.status (201).json(newPost)
+        res.status (201).json(newPost);
         
     } catch (error) {
         res.status(409).json({message: error.message})
@@ -27,6 +28,17 @@ export const createPost = async (req, res) => {
     }
     
 } 
+
+
+export const updatePost = async (req, res) =>{
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID')
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+    res.json(updatedPost);
+
+}
 
 
 //export default getPosts;
